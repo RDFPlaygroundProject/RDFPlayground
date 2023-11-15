0e3ff0ad93c643347c1a728f4a32083f2fa26d93
PJaramilloV <pablojaramillovesperinas@gmail.com>
2023-11-15 12:55:28 -0300

    feat: color nodes

--- mimir/src/main/api/model/SyntaxCheckResponse.kt
+++ mimir/src/main/api/model/SyntaxCheckResponse.kt
@@ -16,6 +16,7 @@ import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryFactory;
 import org.apache.jena.sparql.syntax.Element;
 import org.apache.jena.sparql.syntax.ElementGroup;
+import org.apache.jena.tdb.store.Hash
 
 
 import dot.DOTLang
@@ -68,56 +69,74 @@ class SyntaxCheckController {
 
     fun sparqlPatternToDot(): String {
         // Example SPARQL query string
-        val queryString = """SELECT ?baryon ?comp WHERE {
-            ?baryon a ?Baryon ; ?contains ?blank .
-            ?blank ?component ?comp .
-        }"""
-    
+        val queryString = """
+        SELECT DISTINCT ?baryon ?force 
+        WHERE {
+            ?baryon a ?Baryon ; ?Contains ?_ .
+            ?_ ?Component ?component .
+            ?component ?interaction ?force .
+            } 
+        """
         // Parse the query string into a Query object
         val query = QueryFactory.create(queryString)
     
         // Get the query pattern from the parsed Query object
         val queryPattern: Element = query.queryPattern
     
+        // Map to store unique identifiers for subjects and objects
+        val nodeIds = HashMap<String, Map<String, String> >()
+    
         // Create DOT representation of the query pattern
         val dotRepresentation = StringBuilder("graph QueryPattern {\n")
     
         if (queryPattern is ElementGroup) {
             val group = queryPattern
+            val selectVars = query.projectVars
+            for (variable in selectVars) {
+                val varName = variable.name
+                nodeIds.getOrPut("?$varName", { mapOf("shape" to "ellipse", "color" to "yellow") })
+            }
+
             // Process the elements in the group
             for (el in group.elements) {
-                print("--")
-                println(el)
                 if (el is org.apache.jena.sparql.syntax.ElementPathBlock) {
-                    // Retrieve triple pattern elements
-                    val triple = el.pattern.get(0) // Assuming there's only one triple pattern in the query
+                    val patternBlock = el.pattern
+                    // Loop through all triple patterns in the block
+                    for (triplePath in patternBlock) {
+                        val triple = triplePath.asTriple()
+                        val subject = triple.subject
+                        val obj = triple.`object`
     
-                    // Extract subject, predicate, and object from the triple pattern
-                    val subject = triple.subject
-                    val predicate = triple.predicate
-                    val obj = triple.`object`
+                        // Assign unique IDs to subject and object nodes
+                        var color = "lightblue"
+                        var subject_name = subject.toString()
+                        if (subject_name[1] == '_'){
+                            color = "pink"
+                        }
+                        var obj_name = obj.toString()
+                        nodeIds.getOrPut(subject_name, { mapOf("shape" to "ellipse", "color" to color) })
+                        nodeIds.getOrPut(obj_name, { HashMap<String, String>() })
     
-                    // Add DOT representation for subject node
-                    dotRepresentation.append("  \"${subject}\" [shape=ellipse];\n")
-    
-                    // Add DOT representation for object node
-                    dotRepresentation.append("  \"${obj}\" [shape=ellipse];\n")
-    
-                    // Add DOT representation for edge representing the predicate
-                    dotRepresentation.append("  \"${subject}\" -> \"${obj}\" [label=\"${predicate}\"];\n")
+                        // Add DOT representation for edge representing the predicate
+                        dotRepresentation.append("  \"$subject_name\" -> \"$obj_name\" [label=\"${triple.predicate}\"];\n")
+                    }
                 }
+            }
+            // Add DOT representation for subject node if not already added
+            for ((subject_name, subject) in nodeIds){
+                var shape = subject.get("shape")
+                var color = subject.get("color")
+                dotRepresentation.append("  \"$subject_name\" [shape=$shape, fillcolor=\"$color\", style=\"filled\"];\n")
             }
         }
     
         dotRepresentation.append("}")
     
         // Print DOT representation
-        println("SPARQL Pattern:")
-        println(queryPattern.toString())
         println("DOT Representation:")
         println(dotRepresentation.toString())
     
         return dotRepresentation.toString()
     }
-    
+                
 }
