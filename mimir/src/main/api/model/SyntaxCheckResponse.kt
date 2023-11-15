588c9dc97b7b976c26884502e1fad4596af1664b
PJaramilloV <pablojaramillovesperinas@gmail.com>
2023-11-15 11:58:45 -0300

    experimental query parser

--- mimir/src/main/api/model/SyntaxCheckResponse.kt
+++ mimir/src/main/api/model/SyntaxCheckResponse.kt
@@ -12,6 +12,12 @@ import org.apache.jena.riot.RDFFormat
 import org.apache.jena.riot.RDFLanguages
 import java.io.ByteArrayOutputStream
 
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.sparql.syntax.Element;
+import org.apache.jena.sparql.syntax.ElementGroup;
+
+
 import dot.DOTLang
 import loadModel
 
@@ -46,13 +52,72 @@ class SyntaxCheckController {
 
         // Convert to DOT and save on ByteArray Stream
         val modelOnDOT = ByteArrayOutputStream()
+        val dotRepresentation: String // Store DOT representation
         try {
             // write to DOT
             RDFDataMgr.write(modelOnDOT, model, RDFFormat(DOTLang)) // Writes DOT to modelOnDOT
+
+            // Get DOT representation from sparqlPatternToDot()
+            dotRepresentation = sparqlPatternToDot()
         } catch (e: Exception) {
             return ResponseEntity(SyntaxCheckResponse(syntax_error = e.message.toString()), HttpStatus.INTERNAL_SERVER_ERROR)
         }
-
-        return ResponseEntity(SyntaxCheckResponse(data_dot =  modelOnDOT.toString()), HttpStatus.OK)
+        return ResponseEntity(SyntaxCheckResponse(data_dot =  dotRepresentation), HttpStatus.OK)
     }
+
+
+    fun sparqlPatternToDot(): String {
+        // Example SPARQL query string
+        val queryString = """SELECT ?baryon ?comp WHERE {
+            ?baryon a ?Baryon ; ?contains ?blank .
+            ?blank ?component ?comp .
+        }"""
+    
+        // Parse the query string into a Query object
+        val query = QueryFactory.create(queryString)
+    
+        // Get the query pattern from the parsed Query object
+        val queryPattern: Element = query.queryPattern
+    
+        // Create DOT representation of the query pattern
+        val dotRepresentation = StringBuilder("graph QueryPattern {\n")
+    
+        if (queryPattern is ElementGroup) {
+            val group = queryPattern
+            // Process the elements in the group
+            for (el in group.elements) {
+                print("--")
+                println(el)
+                if (el is org.apache.jena.sparql.syntax.ElementPathBlock) {
+                    // Retrieve triple pattern elements
+                    val triple = el.pattern.get(0) // Assuming there's only one triple pattern in the query
+    
+                    // Extract subject, predicate, and object from the triple pattern
+                    val subject = triple.subject
+                    val predicate = triple.predicate
+                    val obj = triple.`object`
+    
+                    // Add DOT representation for subject node
+                    dotRepresentation.append("  \"${subject}\" [shape=ellipse];\n")
+    
+                    // Add DOT representation for object node
+                    dotRepresentation.append("  \"${obj}\" [shape=ellipse];\n")
+    
+                    // Add DOT representation for edge representing the predicate
+                    dotRepresentation.append("  \"${subject}\" -> \"${obj}\" [label=\"${predicate}\"];\n")
+                }
+            }
+        }
+    
+        dotRepresentation.append("}")
+    
+        // Print DOT representation
+        println("SPARQL Pattern:")
+        println(queryPattern.toString())
+        println("DOT Representation:")
+        println(dotRepresentation.toString())
+    
+        return dotRepresentation.toString()
+    }
+    
 }
